5da7c26a06a0c27b90518030a5722d56e685caa6
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-12-09 19:16:57 -0800

    next

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 //Final Exam - COMSC-210 - Avleen Gill
 
 #include <iostream>
+#include <string>
+#include <cstdlib>
 using namespace std;
 
 //creating customer struct
@@ -8,9 +10,6 @@ struct Node {
     string name;
     string drinkOrder;
     Node* next;
-
-    string names[8] = {"Alice", "Bob", "Ava", "Emily", "John", "Henry", "Frank", "Marc"};
-    string drinks[8] = {"Latte", "Almondmilk Latte", "Green tea", "Iced tea", "Americano", "Caramel Frap", "Caramel Macchiato", "Vanilla latte"};
 
     Node(string n, string d) { name = n; drinkOrder = d; next = nullptr; }
     Node() {name = ""; drinkOrder = ""; next = nullptr;}
@@ -22,8 +21,33 @@ void displayline(Node*&); //call in each round
 
 
 int main() {
+    srand(time(0));
 
-    Node coffeeLine;
+    string names[8] = {"Alice", "Bob", "Ava", "Emily", "John", "Henry", "Frank", "Marc"};
+    string drinks[8] = {"Latte", "Almondmilk Latte", "Green tea", "Iced tea", "Americano", "Caramel Frap", "Caramel Macchiato", "Vanilla latte"};
+
+    Node* coffeeLine;
+    for (int initialq = 0; initialq < 3; initialq++) {
+        string randname = names[rand() % 8];
+        string randdrink = drinks[rand() % 8];
+
+        addcustomer(coffeeLine, randname, randdrink);
+    }
+    int prob = rand() % 100;
+    for (int rounds = 0; rounds < 10; rounds++) {
+        cout << "-------Round " << rounds + 1 << "-------\n";
+
+        if (coffeeLine) {
+            cout << "Serving customer:\n";
+            servecustomer(coffeeLine);
+        }
+        else {
+            cout << "Line is currently empty.\n";
+        }
+        if (prob < 50) {
+            addcustomer(coffeeLine, rand)
+        }
+    }
 
 
 
