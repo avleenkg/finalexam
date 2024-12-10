17d31962544dd63db9fb9bc14cb2a5f88c388817
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-12-09 18:45:43 -0800

    initial

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include <iostream>
 using namespace std;
 
+//creating customer struct
+struct Node {
+    string name;
+    string drinkOrder;
+    Node* next;
+
+    string names[] = {"Alice", "Bob", "Ava", "Emily", "John", "Henry", "Frank", "Marc"};
+    
+};
+
+struct linkedlist
+
 int main() {
 
 
